ffa93b0b00daee2ae06a669e347ffbde43f6bf2f
Harrison Warburton <81275784+dev-harrisonw@users.noreply.github.com>
2022-12-28 22:46:12 +0000

    icon size

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
         <p className="text-xl mb-5">Hi, I'm Harrison.</p>
         <h1 className="heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
           I enjoy <span className="heroShiny1 text-fun-pink">exploring</span> and{" "}
-          <span className="heroShiny2 text-fun-pink">building</span> for the
+          <span className="heroShiny2 text-fun-pink">building</span> the
           web.
           <img
             className="sqD squiggle-hero-html w-16 top-[-90px] right-[5%] sm:top-[-90px] sm:right-[170px]"
