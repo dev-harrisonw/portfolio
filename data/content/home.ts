071671f664bcd2731aa3618668d171437d0f7a28
dev-harrisonw <harrisonwarby111@gmail.com>
2023-01-11 10:42:04 +0000

    text

--- data/content/home.ts
+++ data/content/home.ts
@@ -66,19 +66,19 @@ export const testimonials: Testimonial[] = [
   {
     quote:
       "The communication throughout the project was spot on and the genre of design was carried out well.",
-    name: "",
+    name: "Zak Ali",
     job: "",
   },
   {
     quote:
       "The code was clean, easy to understand, and exactly what I was looking for.",
-    name: "",
+    name: "Connor Brooks",
     job: "",
   },
   {
     quote:
       "My vision was executed perfectly. The quality and speed of the work was excellent.",
-    name: "",
+    name: "Jimmy Thompson",
     job: "",
   },
 ];
